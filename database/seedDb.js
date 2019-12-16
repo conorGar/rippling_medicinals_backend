e35da547bd7cdea53854c24ed7c3ace6c9a651db
Conor Garity <conorgar@gmail.com>
2019-12-16 13:07:18 -0500

    seed with only shannon admin

--- database/seedDb.js
+++ database/seedDb.js
@@ -7,6 +7,7 @@ const main = async () => {
       // empty both tables
       await Admin.destroy({ where: {} })
       await BlogPost.destroy({ where: {} })
+      await Product.destroy({ where: {} })
   
       //user seed
       const Shannon = await Admin.create({
@@ -17,30 +18,30 @@ const main = async () => {
     
   
       //project seed
-      const post1 = await BlogPost.create({
-        title: 'Test Post',
-        description: 'This is first blog post yay',
-        imgUrl: 'https://www.verywellhealth.com/thmb/g6ZvER87sfrNdrmdc00JIgAQOqw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-155098400-5b6102b84cedfd005062a2c5.jpg'
-      })
+      // const post1 = await BlogPost.create({
+      //   title: 'Test Post',
+      //   description: 'This is first blog post yay',
+      //   imgUrl: 'https://www.verywellhealth.com/thmb/g6ZvER87sfrNdrmdc00JIgAQOqw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-155098400-5b6102b84cedfd005062a2c5.jpg'
+      // })
 
 
-      const product1 = await Product.create({
-        title: 'Immune Unity',
-        description: 'Immune unity is formulated to help the systems of our body come into union with the world around us. This formula can help prevent illness by supporting the immune system and our bodies natural process of removing pathogens from the body in a safe and healthy way.',
-        imgUrl: 'https://www.verywellhealth.com/thmb/g6ZvER87sfrNdrmdc00JIgAQOqw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-155098400-5b6102b84cedfd005062a2c5.jpg',
-        ingredients: 'ingredient1, ingredient2, ingredient3'
-      })
+      // const product1 = await Product.create({
+      //   title: 'Immune Unity',
+      //   description: 'Immune unity is formulated to help the systems of our body come into union with the world around us. This formula can help prevent illness by supporting the immune system and our bodies natural process of removing pathogens from the body in a safe and healthy way.',
+      //   imgUrl: 'https://www.verywellhealth.com/thmb/g6ZvER87sfrNdrmdc00JIgAQOqw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-155098400-5b6102b84cedfd005062a2c5.jpg',
+      //   ingredients: 'ingredient1, ingredient2, ingredient3'
+      // })
 
-      const product2 = await Product.create({
-        title: 'Immune Unity',
-        description: 'Immune unity is formulated to help the systems of our body come into union with the world around us. This formula can help prevent illness by supporting the immune system and our bodies natural process of removing pathogens from the body in a safe and healthy way.',
-        imgUrl: 'https://www.verywellhealth.com/thmb/g6ZvER87sfrNdrmdc00JIgAQOqw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-155098400-5b6102b84cedfd005062a2c5.jpg',
-        ingredients: 'ingredient1, ingredient2, ingredient3'
-      })
+      // const product2 = await Product.create({
+      //   title: 'Immune Unity',
+      //   description: 'Immune unity is formulated to help the systems of our body come into union with the world around us. This formula can help prevent illness by supporting the immune system and our bodies natural process of removing pathogens from the body in a safe and healthy way.',
+      //   imgUrl: 'https://www.verywellhealth.com/thmb/g6ZvER87sfrNdrmdc00JIgAQOqw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-155098400-5b6102b84cedfd005062a2c5.jpg',
+      //   ingredients: 'ingredient1, ingredient2, ingredient3'
+      // })
 
      
   
-      await post1.setAdmin(Shannon)
+      // await post1.setAdmin(Shannon)
 
   
     } catch (error) {
